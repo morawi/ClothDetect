7e78e9ae5ef86f70bd5a82ea05acbe98d1183a50
Al-Rawi <ms.alrawi@gmail.com>
2020-03-26 21:28:51 +0000

    Added save model

--- mask_rcnn.py
+++ mask_rcnn.py
@@ -35,7 +35,7 @@ parser.add_argument("--img_height", type=int, default=512, help="size of image h
 parser.add_argument("--img_width", type= int, default=512, help="size of image width")
 
 parser.add_argument("--sample_interval", type=int, default=100, help="interval between sampling of images from generators")
-parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between model checkpoints")
+parser.add_argument("--checkpoint_interval", type=int, default=50, help="interval between model checkpoints")
 parser.add_argument("--HPC_run", type=int, default=0, help="if 1, sets to true if running on HPC: default is 0 which reads to False")
 parser.add_argument("--remove_background", type=int, default=0, help="if 1, sets to true if: default is 1 which reads to False")
 parser.add_argument("--redirect_std_to_file", type =int, default=0, help="set all console output to file: default is 0 which reads to False")
@@ -43,29 +43,27 @@ parser.add_argument("--train_percentage", type=float, default=0.8, help="percent
 
 
 opt = parser.parse_args()
-print(opt)
+
 if platform.system()=='Windows':
     opt.n_cpu= 0
-
 
 opt.train_shuffle = False
 opt.batch_size = 2
 opt.num_epochs = 2
 opt.print_freq = 10
-opt.train_percentage=0.05 # to be used for debugging with low number of samples
+opt.checkpoint_interval=1
+opt.train_percentage=0.02 # to be used for debugging with low number of samples
 
 if platform.system()=='Windows':
     opt.n_cpu= 0
 
+print(opt)
 
 dt = datetime.datetime.today()
 opt.experiment_name = opt.dataset_name+"-mask_rcnn-"+calendar.month_abbr[dt.month]+"-"+str(dt.day)+'-at-'+str(dt.hour) +'-'+str(dt.minute)
 
 os.makedirs("images/%s" % opt.experiment_name, exist_ok=True)
 os.makedirs("saved_models/%s" % opt.experiment_name, exist_ok=True)
-
-
-
 
 
 
@@ -79,7 +77,7 @@ data_loader, data_loader_test = get_dataloaders(opt)
 model = get_model_instance_segmentation( number_of_classes(opt.dataset_name) )    
 if opt.epoch != 0:
     # Load pretrained models
-    model.load_state_dict(torch.load("saved_models/%s/generator_%d.pth" % (opt.dataset_name, opt.epoch)))
+    model.load_state_dict(torch.load("saved_models/%s/maskrcnn__%d.pth" % (opt.dataset_name, opt.epoch)))
    
 model.to(device)
 
@@ -98,12 +96,15 @@ for epoch in range(opt.num_epochs):
     # update the learning rate
     lr_scheduler.step()
     # evaluate on the test dataset    
-    evaluate(model, data_loader_test, device=device)
+    
+    evaluate(model, data_loader, device=device) # used for debugging
+    
+    # evaluate(model, data_loader_test, device=device)
     
     # evaluate(model, data_loader, device=device) # just for debugging
     if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval== 0:
        # Save model checkpoints
-       torch.save(model.state_dict(), "saved_models/%s/generator_%d.pth" % (opt.experiment_name, epoch))
+       torch.save(model.state_dict(), "saved_models/%s/maskrcnn_%d.pth" % (opt.experiment_name, epoch))
        
 
 
