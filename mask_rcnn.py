751da470a82ae72c390751cfef1c5a58bc2c7114
Al-Rawi <ms.alrawi@gmail.com>
2020-03-28 03:44:23 +0000

    Added save, load, and sample images

--- mask_rcnn.py
+++ mask_rcnn.py
@@ -8,17 +8,17 @@ Created on Tue Mar 24 13:35:25 2020
 # https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 
 # from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-# from engine import train_one_epoch, evaluate
-import torch
 from datasets import number_of_classes
 from models import get_model_instance_segmentation
+from engine import train_one_epoch, evaluate
 from misc_utils import get_dataloaders
+import torch
 import argparse
 import platform
-from engine import train_one_epoch, evaluate
 import datetime
 import calendar
 import os
+import sys
 
 
 parser = argparse.ArgumentParser()
@@ -40,6 +40,7 @@ parser.add_argument("--HPC_run", type=int, default=0, help="if 1, sets to true i
 parser.add_argument("--remove_background", type=int, default=0, help="if 1, sets to true if: default is 1 which reads to False")
 parser.add_argument("--redirect_std_to_file", type =int, default=0, help="set all console output to file: default is 0 which reads to False")
 parser.add_argument("--train_percentage", type=float, default=0.8, help="percentage of samples used in training, the rest used for testing")
+parser.add_argument("--experiment_name", type=str, default=None, help="name of the folder inside saved_models")
 
 
 opt = parser.parse_args()
@@ -53,18 +54,36 @@ opt.num_epochs = 2
 opt.print_freq = 10
 opt.checkpoint_interval=1
 opt.train_percentage=0.02 # to be used for debugging with low number of samples
+opt.epoch=0
+opt.experiment_name = 'ClothCoParse-mask_rcnn-Mar-26-at-21-2'
+
+
+def sample_images(data_loader_test, model, device):
+    images,targets = next(iter(data_loader_test)) # grab the images
+    images = list(image.to(device) for image in images)
+    model.eval()  # setting model to evaluation mode
+    predictions = model(images)           # Returns predictions
+    model.train() # putting back the model into train status/mode 
+    ''' TODO: Do something with the predictions, ie display / save '''
+    
+
 
 if platform.system()=='Windows':
-    opt.n_cpu= 0
+    opt.n_cpu= 0 
 
 print(opt)
 
-dt = datetime.datetime.today()
-opt.experiment_name = opt.dataset_name+"-mask_rcnn-"+calendar.month_abbr[dt.month]+"-"+str(dt.day)+'-at-'+str(dt.hour) +'-'+str(dt.minute)
-
-os.makedirs("images/%s" % opt.experiment_name, exist_ok=True)
-os.makedirs("saved_models/%s" % opt.experiment_name, exist_ok=True)
-
+# sanity check
+if opt.epoch !=0 and opt.experiment_name is None:
+    print('When epohch is not 0, there should be the name of the folder that has previously stored model')
+    sys.exit()
+elif opt.epoch !=0 and opt.experiment_name is not None:
+    opt.experiment_name = opt.dataset_name +'/'+ opt.experiment_name
+else: # totaly new experiment        
+    dt = datetime.datetime.today()
+    opt.experiment_name = opt.dataset_name+'/'+"-mask_rcnn-"+calendar.month_abbr[dt.month]+"-"+str(dt.day)+'-at-'+str(dt.hour) +'-'+str(dt.minute)
+    os.makedirs("images/%s" % opt.experiment_name, exist_ok=True)
+    os.makedirs("saved_models/%s" % opt.experiment_name, exist_ok=True)
 
 
 # train on the GPU or on the CPU, if a GPU is not available
@@ -77,7 +96,8 @@ data_loader, data_loader_test = get_dataloaders(opt)
 model = get_model_instance_segmentation( number_of_classes(opt.dataset_name) )    
 if opt.epoch != 0:
     # Load pretrained models
-    model.load_state_dict(torch.load("saved_models/%s/maskrcnn__%d.pth" % (opt.dataset_name, opt.epoch)))
+    print("loading model %s maskrcnn_%d.pth" % (opt.experiment_name, opt.epoch) )        
+    model.load_state_dict(torch.load("saved_models/%s/maskrcnn_%d.pth" % (opt.experiment_name, opt.epoch)))
    
 model.to(device)
 
@@ -91,6 +111,7 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
 
 
 for epoch in range(opt.num_epochs):
+    
     # train for one epoch, printing every 10 iterations
     train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=opt.print_freq)
     # update the learning rate
@@ -101,24 +122,22 @@ for epoch in range(opt.num_epochs):
     
     # evaluate(model, data_loader_test, device=device)
     
-    # evaluate(model, data_loader, device=device) # just for debugging
+    
     if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval== 0:
        # Save model checkpoints
        torch.save(model.state_dict(), "saved_models/%s/maskrcnn_%d.pth" % (opt.experiment_name, epoch))
        
 
+sample_images(data_loader_test, model, device)
 
 print("All done")
 
-# images,targets = next(iter(data_loader))
-# device = torch.device('cpu')
-# # images= images.to(device)
+
+
+# images,targets = next(iter(data_loader_test))
 # images = list(image.to(device) for image in images)
 # targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 # output = model(images,targets)   # Returns losses and detections
 # # For inference
-# model.eval()
-# x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
-# predictions = model(x)           # Returns predictions
-
-
+# model.eval()  # probably not needed here 
+# predictions = model(images)
